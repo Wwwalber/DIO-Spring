b13907b767be3f04f6f7b0c604e086aded0b5906
walber <walberoc@gmail.com>
2024-07-16 20:13:28 -0300

    funcionando .. mas ainda possui um new (precisamos eliminá-lo)

--- primeiros-passos/src/main/java/com/dio/primeiros_passos/MyApp.java
+++ primeiros-passos/src/main/java/com/dio/primeiros_passos/MyApp.java
@@ -1,5 +1,16 @@
 package com.dio.primeiros_passos;
 
-public class MyApp {
-    
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApp implements CommandLineRunner {
+
+    private Calculadora calculadora; // informo que vou precisar disso
+
+    @Override
+    public void run(String... args) throws Exception {
+        Calculadora calculadora = new Calculadora();
+        System.out.println("O resultado é "+ calculadora.somar(2,7));
+    }
 }
