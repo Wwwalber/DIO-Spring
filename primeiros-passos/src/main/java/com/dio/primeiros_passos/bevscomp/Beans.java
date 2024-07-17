54ce94ab789944995fab6c3aa7921c626150baae
walber <walberoc@gmail.com>
2024-07-16 21:27:24 -0300

    para algumas versões será necessário acrescentar mais essa liinha

--- primeiros-passos/src/main/java/com/dio/primeiros_passos/bevscomp/Beans.java
+++ primeiros-passos/src/main/java/com/dio/primeiros_passos/bevscomp/Beans.java
@@ -1,9 +1,10 @@
 package com.dio.primeiros_passos.bevscomp;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.google.gson.Gson;
-
+@Configuration
 public class Beans {  
     @Bean // agora pose ser injetado em qualquer parte da aplicação
     public Gson gson(){
