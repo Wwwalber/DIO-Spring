595d088835e0363edc3de16fa386599ad00f3504
walber <walberoc@gmail.com>
2024-07-19 18:35:15 -0300

    update usando aplication.properties

--- primeiros-passos/src/main/java/com/dio/aplproperties/SistemaMensagem.java
+++ primeiros-passos/src/main/java/com/dio/aplproperties/SistemaMensagem.java
@@ -1,18 +1,19 @@
 package com.dio.aplproperties;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class SistemaMensagem implements CommandLineRunner{
-
-    private String nome = "WALBER COSTA";
-    private String email = "walberoc@gmail.com";
-    private List<Long> telefones = 
-        new ArrayList<>(Arrays.asList(new Long[] {115255484888L}));
+    // por expresssion
+    @Value("${nome}")
+    private String nome;
+    @Value("${email}")
+    private String email;
+    @Value("${telefones}")
+    private List<Long> telefones;
 
     @Override
     public void run(String... args) throws Exception {
