87b0d9928d990b9bb70782b030cd1356a54b88e8
walber <walberoc@gmail.com>
2024-07-21 19:52:53 -0300

    chamada ok passando parâmetro na uri ../users/gleyson

--- first-web-api/src/main/java/dio/first_web_api/controller/UsuarioController.java
+++ first-web-api/src/main/java/dio/first_web_api/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.first_web_api.model.Usuario;
@@ -18,5 +19,10 @@ public class UsuarioController {  //será como um proxi, não terá regras de ne
     @GetMapping("/users") // retorna todos os usuarios
     public List<Usuario> getUsers(){
         return repository.findAll();
+    }
+
+    @GetMapping("/users/{username}") // usar parâmetro vindo da uri
+    public Usuario getOne(@PathVariable("username") String username){
+        return repository.findByUsername(username);
     }
 }
