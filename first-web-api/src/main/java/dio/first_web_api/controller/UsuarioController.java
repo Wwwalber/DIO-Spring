c78b4514a927821362a3ac158a0a8f5002129dc4
walber <walberoc@gmail.com>
2024-07-21 20:33:05 -0300

    testes com a API ok

--- first-web-api/src/main/java/dio/first_web_api/controller/UsuarioController.java
+++ first-web-api/src/main/java/dio/first_web_api/controller/UsuarioController.java
@@ -3,12 +3,16 @@ package dio.first_web_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.first_web_api.model.Usuario;
 import dio.first_web_api.repository.UsuarioRepository;
+
 
 @RestController
 public class UsuarioController {  //será como um proxi, não terá regras de negócios
@@ -25,4 +29,15 @@ public class UsuarioController {  //será como um proxi, não terá regras de ne
     public Usuario getOne(@PathVariable("username") String username){
         return repository.findByUsername(username);
     }
+
+    @DeleteMapping("users/{id}") // http só reliza no navegador gets, para deletar parâmetropreciso de um cliente http
+    public void delete(@PathVariable("id") Integer id){ //
+        repository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public void postUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+
 }
