a0adb24f1ef13ff6eaa1538f142a3a951d5b951d
walber <walberoc@gmail.com>
2024-07-21 20:36:16 -0300

    centralizando chamada @RequestMapping("/users")

--- first-web-api/src/main/java/dio/first_web_api/controller/UsuarioController.java
+++ first-web-api/src/main/java/dio/first_web_api/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.first_web_api.model.Usuario;
@@ -15,27 +16,28 @@ import dio.first_web_api.repository.UsuarioRepository;
 
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {  //será como um proxi, não terá regras de negócios
     
     @Autowired //injetado
     private UsuarioRepository repository;
 
-    @GetMapping("/users") // retorna todos os usuarios
+    @GetMapping("") // retorna todos os usuarios
     public List<Usuario> getUsers(){
         return repository.findAll();
     }
 
-    @GetMapping("/users/{username}") // usar parâmetro vindo da uri
+    @GetMapping("/{username}") // usar parâmetro vindo da uri
     public Usuario getOne(@PathVariable("username") String username){
         return repository.findByUsername(username);
     }
 
-    @DeleteMapping("users/{id}") // http só reliza no navegador gets, para deletar parâmetropreciso de um cliente http
+    @DeleteMapping("/{id}") // http só reliza no navegador gets, para deletar parâmetropreciso de um cliente http
     public void delete(@PathVariable("id") Integer id){ //
         repository.deleteById(id);
     }
 
-    @PostMapping("/users")
+    @PostMapping("")
     public void postUser(@RequestBody Usuario usuario){
         repository.save(usuario);
     }
